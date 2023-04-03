51448b656f1d6c573d9a09819b26d40d5de677a7
242484 Olejniczak Jakub <242484@edu.p.lodz.pl>
2023-04-03 18:43:59 +0200

    numerki naprawione

--- Numerki/Zad2/algorytm.py
+++ Numerki/Zad2/algorytm.py
@@ -1,37 +1,36 @@
-def jordan(macierz, wektor):
-    liczba_wierszy = len(wektor)
-    for k in range(liczba_wierszy):
-        for wiersz in macierz:
-            wiersz_kopia = [wartosc for wartosc in wiersz]
-            if wiersz_kopia == [0] * liczba_wierszy:
-                wektor_kopia = [wartosc for wartosc in wektor]
-                if wektor_kopia[macierz.index(wiersz)] == 0:
+def jordan(matrix, vector):
+    rows = len(vector)
+    for k in range(rows):
+        for row in matrix:
+            row_copy = [round(value, 8) for value in row]
+            if row_copy == [0] * rows:
+                vector_copy = [round(value, 8) for value in vector]
+                if vector_copy[matrix.index(row)] == 0:
                     print("Układ nieoznaczony")
                 else:
                     print("Układ sprzeczny")
                 return None
-        maks = abs(macierz[k][k])
+        max = abs(matrix[k][k])
         index = k
-        for i in range(liczba_wierszy - k):
-            if abs(macierz[k + i][k]) > maks:
-                maks = abs(macierz[k + i][k])
+        for i in range(rows - k):
+            if abs(matrix[k + i][k]) > max:
+                max = abs(matrix[k + i][k])
                 index = k + i
         if index != k:
-            for i in range(liczba_wierszy):
-                macierz[k][i], macierz[index][i] = macierz[index][i], macierz[k][i]
-            wektor[index], wektor[k] = wektor[k], wektor[index]
-        akk = macierz[k][k]
-        wektor[k] /= akk
-        for j in range(liczba_wierszy - k):
-            macierz[k][k + j] /= akk
-        for i in range(liczba_wierszy):
+            matrix[k], matrix[index] = matrix[index], matrix[k]
+            vector[index], vector[k] = vector[k], vector[index]
+        akk = matrix[k][k]
+        vector[k] /= akk
+        for j in range(rows - k):
+            matrix[k][k + j] /= akk
+        for i in range(rows):
             if i != k:
-                aik = macierz[i][k]
-                wektor[i] -= wektor[k] * aik
-                for j in range(liczba_wierszy):
-                    macierz[i][j] -= macierz[k][j] * aik
-    wektor = [round(x, 8) for x in wektor]
-    return wektor
+                aik = matrix[i][k]
+                vector[i] -= vector[k] * aik
+                for j in range(rows):
+                    matrix[i][j] -= matrix[k][j] * aik
+    vector = [round(x, 8) for x in vector]
+    return vector
 
 
 def get_matrix(text_string):
