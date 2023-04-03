0c3b9ea8818cfc348f6e1f59f6e8ae27f8488973
242377 Deka Krzysztof <242377@edu.p.lodz.pl>
2023-04-03 15:25:49 +0200

    krypto

--- Kryptografia/AES/src/main/java/com/example/aes/HelloApplication.java
+++ Kryptografia/AES/src/main/java/com/example/aes/HelloApplication.java
@@ -12,6 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        scene.getStylesheets().add("stylesheet.css");
         stage.setTitle("AES encrypting");
         stage.setScene(scene);
         stage.show();
