e8abbef2fad5b5f88d7fef71f5c9da7a3768ddb4
242377 Deka Krzysztof <242377@edu.p.lodz.pl>
2023-04-03 15:44:37 +0200

    downsizing

--- Kryptografia/AES/src/main/java/com/example/aes/HelloApplication.java
+++ Kryptografia/AES/src/main/java/com/example/aes/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        Scene scene = new Scene(fxmlLoader.load(), 1344, 756);
         scene.getStylesheets().add("stylesheet.css");
         stage.setTitle("AES encrypting");
         stage.setScene(scene);
