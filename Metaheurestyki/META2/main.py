87db1e63888aa7aca780ffb5a90cb9d899cf393c
242484 Olejniczak Jakub <242484@edu.p.lodz.pl>
2023-11-16 12:57:14 +0100

    dodanie drugiego zadania AJI do folderu AJI2

--- Metaheurestyki/META2/main.py
+++ Metaheurestyki/META2/main.py
@@ -23,32 +23,54 @@ def f4(x):
     return x * math.sin(10 * math.pi * x) + 1
 
 
-def simulated_annealing(funkcja, min_val, max_val, T, a, M):
-    x = random.uniform(min_val, max_val)
-    for j in range(M):
-        x2 = random_value(x, min_val, max_val, T)
+def simulated_annealing(funkcja, T):
+    if funkcja == f3:
+        min_val = -150.0
+        max_val = 150.0
+        a = 0.999
+        M = 3000
+    elif funkcja == f4:
+        min_val = -1.0
+        max_val = 2.0
+        a = 0.997
+        M = 1200
 
-        if funkcja(x2) > funkcja(x):
-            x = x2
-        else:
-            if math.exp(-(funkcja(x) - funkcja(x2)) / (0.1 * T)) > random.random():
+    x_values = []
+    for i in range(5):
+        x = random.uniform(min_val, max_val)
+        for j in range(M):
+            x2 = random_value(x, min_val, max_val, T)
+
+            if funkcja(x2) > funkcja(x):
                 x = x2
-        T = T * a
+            else:
+                if math.exp(-(funkcja(x) - funkcja(x2)) / (0.1 * T)) > random.random():
+                    x = x2
+            T = T * a
+        x_values.append(x)
 
-    return x
+    return x_values
 
 
+temp_list = [50, 100, 200, 300, 400]
+temp_list2 = [0.5, 1, 2, 3, 4]
+x_values = []
+print(temp_list)
 choice = int(input("Choose function (1 for f3, 2 for f4): "))
-min_val = float(input("Enter the lower bound: "))
-max_val = float(input("Enter the upper bound: "))
-T = float(input("Enter the starting temperature: "))
-a = float(input("Enter the cooling parameter: "))
-M = int(input("Enter the number of iterations: "))
 
-if choice == 1:
-    x = simulated_annealing(f3, min_val, max_val, T, a, M)
-else:
-    x = simulated_annealing(f4, min_val, max_val, T, a, M)
+for i in range(5):
+    if choice == 1:
+        print(temp_list)
+        x_values = simulated_annealing(f3, temp_list[i])
+        print(f'Optimal x: {x_values}')
+        for x in range(5):
+            print(f3(x_values[x]))
+    else:
+        print(temp_list2)
+        x_values = simulated_annealing(f4, temp_list2[i])
+        print(f'Optimal x: {x_values}')
+        for x in range(5):
+            print(f4(x_values[x]))
+    print(".........")
 
-print(f'Optimal x: {x}')
-print(f'Function value at x: {f3(x) if choice == 1 else f4(x)}')
+
