12fa09101ff6d60514a1df889ec7d4196f4687be
242377 Deka Krzysztof <242377@edu.p.lodz.pl>
2023-11-23 19:56:05 +0100

    Sortowanie

--- AJI/AJI3/aji3/src/components/ByCast.vue
+++ AJI/AJI3/aji3/src/components/ByCast.vue
@@ -1,14 +1,35 @@
 <template>
     <div>
         <h1>Filmy wg obsady</h1>
-        <div v-for="(movies, cast) in moviesByCast" :key="cast">
-            <h2>{{ cast }}</h2>
-            <ul style="list-style-type: decimal;">
-                <li v-for="movie in movies" :key="movie.title">
-                    {{ movie.title }}
-                </li>
-            </ul>
-        </div>
+        <template v-if="!isSorting">
+            <button @click="toggleSorting">Sortuj A-Z</button>
+        </template>
+        <template v-else>
+            <button @click="toggleSorting">Wyłącz sortowanie</button>
+        </template>
+
+
+        <template v-if="!isSorting">
+            <div v-for="(movies, cast) in moviesByCast" :key="cast">
+                <h2>{{ cast }}</h2>
+                <ul style="list-style-type: decimal;">
+                    <li v-for="movie in movies" :key="movie.title">
+                        {{ movie.title }}
+                    </li>
+                </ul>
+            </div>
+        </template>
+
+        <template v-else>
+            <div v-for="(movies, cast) in sortedMovies" :key="cast">
+                <h2>{{ cast }}</h2>
+                <ul style="list-style-type: decimal;">
+                    <li v-for="movie in movies" :key="movie.title">
+                        {{ movie.title }}
+                    </li>
+                </ul>
+            </div>
+        </template>
     </div>
 </template>
   
@@ -20,11 +41,18 @@ export default {
     data() {
         return {
             movies: moviesData.slice(199, 300),
-
+            moviesByCast: {},
+            sortedMovies: {},
+            isSorting: false,
         };
     },
-    computed: {
-        moviesByCast() {
+
+    created() {
+        this.updateMovies();
+    },
+
+    methods: {
+        updateMovies() {
             const moviesByCast = {};
 
             _.each(this.movies, (movie) => {
@@ -38,7 +66,23 @@ export default {
                 }
             });
 
-            return moviesByCast;
+            this.moviesByCast = moviesByCast;
+        },
+        sortMovies() {
+
+            _.each(this.moviesByCast, (movies, cast) => {
+                this.moviesByCast[cast] = _.sortBy(movies, (movie) => movie.title.toLowerCase());
+            });
+            this.sortedMovies = _.clone(this.moviesByCast);
+        },
+        toggleSorting() {
+            this.isSorting = !this.isSorting;
+
+            if (this.isSorting) {
+                this.sortMovies();
+            } else {
+                this.updateMovies();
+            }
         },
     },
 
--- AJI/AJI3/aji3/src/components/ByGenre.vue
+++ AJI/AJI3/aji3/src/components/ByGenre.vue
@@ -1,16 +1,38 @@
 <template>
     <div>
         <h1>Filmy wg gatunku</h1>
-        <div v-for="(movies, genre) in moviesByGenre" :key="genre">
-            <h2>{{ genre }}</h2>
-            <ul style="list-style-type: decimal;">
-                <li v-for="movie in movies" :key="movie.title">
-                    {{ movie.title }}
-                </li>
-            </ul>
-        </div>
+        <template v-if="!isSorting">
+            <button @click="toggleSorting">Sortuj A-Z</button>
+        </template>
+        <template v-else>
+            <button @click="toggleSorting">Wyłącz sortowanie</button>
+        </template>
+
+
+        <template v-if="!isSorting">
+            <div v-for="(movies, genre) in moviesByGenre" :key="genre">
+                <h2>{{ genre }}</h2>
+                <ul style="list-style-type: decimal;">
+                    <li v-for="movie in movies" :key="movie.title">
+                        {{ movie.title }}
+                    </li>
+                </ul>
+            </div>
+        </template>
+
+        <template v-else>
+            <div v-for="(movies, genre) in sortedMovies" :key="genre">
+                <h2>{{ genre }}</h2>
+                <ul style="list-style-type: decimal;">
+                    <li v-for="movie in movies" :key="movie.title">
+                        {{ movie.title }}
+                    </li>
+                </ul>
+            </div>
+        </template>
     </div>
 </template>
+  
   
 <script>
 import moviesData from '../assets/movies.json';
@@ -21,10 +43,16 @@ export default {
     data() {
         return {
             movies: moviesData.slice(0, 100),
+            moviesByGenre: {},
+            sortedMovies: {},
+            isSorting: false,
         };
     },
-    computed: {
-        moviesByGenre() {
+    created() {
+        this.updateMovies();
+    },
+    methods: {
+        updateMovies() {
             const moviesByGenre = {};
 
             _.each(this.movies, (movie) => {
@@ -38,8 +66,25 @@ export default {
                 }
             });
 
-            return moviesByGenre;
+            this.moviesByGenre = moviesByGenre;
+        },
+        sortMovies() {
+
+            _.each(this.moviesByGenre, (movies, genre) => {
+                this.moviesByGenre[genre] = _.sortBy(movies, (movie) => movie.title.toLowerCase());
+            });
+            this.sortedMovies = _.clone(this.moviesByGenre);
+        },
+        toggleSorting() {
+            this.isSorting = !this.isSorting;
+
+            if (this.isSorting) {
+                this.sortMovies();
+            } else {
+                this.updateMovies();
+            }
         },
     },
 };
 </script>
+  
