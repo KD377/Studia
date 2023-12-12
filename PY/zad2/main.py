7ad6cb90709a157f0c2cfc3ecba10637e41fb45b
242377 Deka Krzysztof <242377@edu.p.lodz.pl>
2023-12-12 22:58:16 +0100

    python

--- PY/zad2/main.py
+++ PY/zad2/main.py
@@ -33,18 +33,15 @@ def main(config_file=None, log_level=None, rounds=50, number_of_sheep=15, wait=F
         wolf_move_dist = float(config['Wolf']['MoveDist'])
 
         if sheep_init_pos_limit <= 0 or sheep_move_dist <= 0 or wolf_move_dist <= 0 or rounds <= 0 or number_of_sheep <= 0:
-            logging.error("All values must be positive numbers")
-            return
+            raise ValueError("All values must be positive numbers")
         logging.debug(
             f"Values loaded from configuration file: Sheep InitPosLimit = {sheep_init_pos_limit}, Sheep MoveDist = {sheep_move_dist}, Wolf MoveDist = {wolf_move_dist}")
 
-        # Use values from the config file
         meadow_instance = meadow.Meadow(number_of_sheep, rounds, sheep_init_pos_limit, sheep_move_dist, wolf_move_dist)
         meadow_instance.simulation(wait)
     else:
         if rounds <= 0 or number_of_sheep <= 0:
-            logging.error("All values must be positive numbers")
-            return
+            raise ValueError("All values must be positive numbers")
         meadow_instance = meadow.Meadow(number_of_sheep, rounds)
         meadow_instance.simulation(wait)
 
