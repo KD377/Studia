cd61f7c934c3d8b8c6b3d7627dd3c5364edbf3b9
242484 Olejniczak Jakub <242484@edu.p.lodz.pl>
2024-01-03 23:44:46 +0100

    meta5 skonczony program

--- Metaheurestyki/META5/main.py
+++ Metaheurestyki/META5/main.py
@@ -2,7 +2,7 @@ import random
 import math
 
 
-def bunkin_dunction_n6(x, y):
+def bunkin_function_n6(x, y):
     return 100 * math.sqrt(abs(y - 0.01 * x ** 2)) + 0.01 * abs(x + 10)
 
 
@@ -10,7 +10,6 @@ def booth_function(x, y):
     return (x + 2 * y - 7) ** 2 + (2 * x + y - 5) ** 2
 
 
-# Klasa reprezentująca cząstkę
 class Czastka:
     def __init__(self, min_x, max_x, min_y, max_y):
         self.x = random.uniform(min_x, max_x)  # Losowa pozycja x
@@ -23,7 +22,7 @@ class Czastka:
 
 
 # PSO
-def pso(liczba_czastek, liczba_iteracji, min_x, max_x, min_y, max_y, funkcja):
+def pso(liczba_czastek, liczba_iteracji, min_x, max_x, min_y, max_y, funkcja, waga_inercji, ws_poznawczy, ws_spoleczny):
     global najlepszy_wynik_globalny, najlepsze_x_globalne, najlepsze_y_globalne
 
     czastki = [Czastka(min_x, max_x, min_y, max_y) for _ in range(liczba_czastek)]
@@ -42,11 +41,10 @@ def pso(liczba_czastek, liczba_iteracji, min_x, max_x, min_y, max_y, funkcja):
                 najlepsze_x_globalne = p.x
                 najlepsze_y_globalne = p.y
 
-        # inercja 0.2 wspolczynnik poznawczy 0.35 wspolczynnik spoleczny 0.35 z wykladu
         for p in czastki:
-            p.vx = p.vx * 0.2 + 0.35 * random.random() * (p.najlepsze_x - p.x) + 0.45 * random.random() * (
+            p.vx = p.vx * waga_inercji + ws_poznawczy * random.random() * (p.najlepsze_x - p.x) + ws_spoleczny * random.random() * (
                     najlepsze_x_globalne - p.x)
-            p.vy = p.vy * 0.2 + 0.35 * (p.najlepsze_y - p.y) + 0.45 * random.random() * (
+            p.vy = p.vy * waga_inercji + ws_poznawczy * (p.najlepsze_y - p.y) + ws_spoleczny * random.random() * (
                     najlepsze_y_globalne - p.y)
             p.x = p.x + p.vx
             p.y = p.y + p.vy
@@ -57,8 +55,10 @@ def pso(liczba_czastek, liczba_iteracji, min_x, max_x, min_y, max_y, funkcja):
 # Przykładowe użycie PSO
 liczba_czastek = 75
 liczba_iteracji = 100
-#najlepsze_x, najlepsze_y, najlepszy_wynik = pso(liczba_czastek, liczba_iteracji, -15, -5, -3, 3, bunkin_dunction_n6)
-najlepsze_x, najlepsze_y, najlepszy_wynik = pso(liczba_czastek, liczba_iteracji, -10, 10, -10, 10, booth_function)
-print(f"Najlepsze x: {najlepsze_x}")
-print(f"Najlepsze y: {najlepsze_y}")
-print(f"Najlepszy wynik: {najlepszy_wynik}")
+
+for i in range(3):
+    najlepsze_x, najlepsze_y, najlepszy_wynik = pso(liczba_czastek, liczba_iteracji, -15, -5, -3, 3, bunkin_function_n6, 0.5, 1, 1)
+    #najlepsze_x, najlepsze_y, najlepszy_wynik = pso(liczba_czastek, liczba_iteracji, -10, 10, -10, 10, booth_function, 0.5, 1, 1)
+    print(f"Najlepsze x: {najlepsze_x.__round__(4)}")
+    print(f"Najlepsze y: {najlepsze_y.__round__(4)}")
+    print(f"Najlepszy wynik: {najlepszy_wynik.__round__(4)}")
