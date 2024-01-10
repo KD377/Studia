2ebf0cd6a458f254cf82c928ede60af8cff998b8
242377 Deka Krzysztof <242377@edu.p.lodz.pl>
2024-01-10 23:05:31 +0100

    all requirements for grade 4 satisfied

--- Kryptografia/AES/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.aes {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens com.example.aes to javafx.fxml;
-    exports com.example.aes;
-}
